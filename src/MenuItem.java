8510676834ca1fe1bb3c27dc62c7bb1d331e89e1
xg6spwjsjq-cell <xg6spwjsjq@privaterelay.appleid.com>
2026-03-24 09:22:02 +0100

    Add Code To MenuItem

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
-
 public class MenuItem {
+    private String code;
+    private String name;
+    private double price;
+    private String category;
+    private static int productCount = 0;
+
+    public MenuItem(String code, String name, double price, String category) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        productCount++;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public static int getProductCount() {
+        return productCount;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuItem{" +
+                "code='" + code + '\'' +
+                ", name'" + name + '\'' +
+                ", price'" + price + '\'' +
+                ", category'" + category + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        MenuItem menuItem = (MenuItem) obj;
+        return this.code.equals(menuItem.code);
+    }
 }
